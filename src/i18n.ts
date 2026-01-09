84bb990c6b259a59bafddf14be5c91d8575358c5
xx <2430278564@qq.com>
2026-01-09 21:31:19 +0800

    feat:11

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,6 +1,6 @@
 import { createI18n } from 'vue-i18n'
-import en from './locales/en.js'
-import zh from './locales/zh.js'
+import en from './locales/en'
+import zh from './locales/zh'
 const savedLocale = localStorage.getItem('user-locale') || 'zh'
 
 const i18n = createI18n({
